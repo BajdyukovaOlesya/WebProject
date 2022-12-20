3cc0ed783df4c0c4eda5e0041aaab1ffc7c31fe3
Olesja <bajdolesya@yandex.ru>
2022-12-20 17:07:16 +0500

    Создан шаблон страницы для определенной книги

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
+import jinja2 
 
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder="templates")
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Minerva17@localhost/Library"
 db = SQLAlchemy(app)
 
 
-with app.app_context(): book =  db.Table("Books",db.metadata,autoload=True,autoload_with=db.engine)
-
-
-with app.app_context():u=db.session.query(book).all()
-print(u[0].Номер)
-
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template("index.html")
+
+
+@app.route('/1984')
+def inf():
+	b =  db.Table("Books",db.metadata,autoload=True,autoload_with=db.engine)
+	u=db.session.query(b).all()
+	book=u[0]
+	return render_template('base.html',book=book)
+
 
 
 if __name__=="__main__": app.run(debug=True)
